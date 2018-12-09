79ba3f1534ccfcbc02f1fb9dcfba2d80aa4bb1c0
jubayera <cajubuvic@gmail.com>
2018-12-08 17:13:47 -0800

    Update array_process.cpp

--- array_process.cpp
+++ array_process.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+
 using namespace std;
 
 //Generates number (from 1 - 100). Print number of occurrences of each number.
@@ -46,6 +48,26 @@ void getInts( vector<int> & array )
 	array.resize( itemsRead );
 }
 
+/*
+ * Read an unlimited number of ints with no attempts at error recovery; fill the vector parameter with the data;
+ * its size aftere the return tells how many items were read.
+ */
+void getInts_using_push_back( vector<int> & array )
+{
+	int inputVal;
+
+	array.resize( 0 );
+
+	cout << "Enter any number of integers: ";
+	/*
+	 * Code to read an unlimited number of ints and write them out using push_back
+	 */
+	while( cin >> inputVal )
+	{
+		array.push_back( inputVal );
+	}
+}
+
 //Code to read an unlimited number of ints and write them out, using array-doubling
 void read_ints_any_number_times(void)
 {
@@ -57,11 +79,23 @@ void read_ints_any_number_times(void)
 		cout << array[i] << endl;
 }
 
+//int findMax( vector<int> & a);
+
+//int findMax( const vector<int> & a);
+
 int main()
 {
+	const int DAYS_IN_MONTH[] = { 31, 28, 31, 30, 31, 30,
+								  31, 31, 30, 31, 30, 31};
+
+	const int NUM_MONTHS = sizeof(DAYS_IN_MONTH) / sizeof(DAYS_IN_MONTH[0]);
+
 	number_of_occurrence();
 
 	read_ints_any_number_times();
+
+	vector<int> array_local;
+	getInts_using_push_back( array_local );
 
 	return 0;
 }
