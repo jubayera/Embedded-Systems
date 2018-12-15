65145a21112e234d5f69f890f61fd9bb18f80579
jubayera <cajubuvic@gmail.com>
2018-12-15 11:57:39 -0800

    Update loop_count_table.cpp

--- loop_count_table.cpp
+++ loop_count_table.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
+#include <string>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
+void count_table(void)
 {
     int n;
     cin >> n;
@@ -10,6 +12,45 @@ int main()
 
     for(int i = 1; i <= 10; i++)
         cout << n << " x " << i << " = " << n*i << endl;
+}
 
-    return 0;
+void printEvenIndexChar(string strparam, int len) {
+  for (int i = 0; i < len; i++) {
+    if (i % 2 == 0) {
+      cout << strparam[i];
+    }
+  }
+}
+
+void printOddIndexChar(string strparam, int len) {
+  for (int i = 0; i < len; i++) {
+    if (i % 2 != 0) {
+      cout << strparam[i];
+    }
+  }
+}
+
+int main() {
+
+  count_table();
+
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  int T;
+  cin >> T;
+
+  string str;
+
+  while (T > 0) {
+    cin >> str;
+    int length = str.length();
+
+    printEvenIndexChar(str, length);
+    cout << " ";
+    printOddIndexChar(str, length);
+    cout << endl;
+
+    T--;
+  }
+
+  return 0;
 }
