fbb8e001bc66ed370db44ed75c0209210ac51217
jubayera <cajubuvic@gmail.com>
2018-12-14 21:52:46 -0800

    Create datatype_int_double_string_get.cpp

---
+++ datatype_int_double_string_get.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() 
+{
+    int i = 4;
+    double d = 4.0;
+    string s = "Init ";
+    
+    // Declare second integer, double, and String variables.
+    int j;
+    double e;
+    string t;
+
+    // Read and save an integer, double, and String to your variables.
+    cin >> j;
+    cin >> e;
+    cin.get();
+    getline(cin, t);
+    // Print the sum of both integer variables on a new line.
+    cout << i + j << endl;
+    // Print the sum of the double variables on a new line.
+    d = d + e;
+    cout << std::fixed << std::setprecision(1) << d << endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s << t;
+
+    return 0;
+}
