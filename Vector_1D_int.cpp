2e6439175f305184d6e0165e9cd5d0b278e9ddd4
jubayera <cajubuvic@gmail.com>
2018-12-16 21:13:40 -0800

    Update Vector_1D_int.cpp

--- Vector_1D_int.cpp
+++ Vector_1D_int.cpp
@@ -6,9 +6,31 @@
 using namespace std; 
    
 // The vect here is a copy of vect in main() 
-void func(vector<int> vect) 
+void func_by_value(vector<int> vect) 
 { 
    vect.push_back(30); 
+} 
+
+// The vect is passed by reference and changes 
+// made here reflect in main() 
+void func_reference(vector<int> &vect) 
+{ 
+   vect.push_back(30); 
+} 
+
+//If we do not want a function to modify a vector, we can pass it as a const reference.
+// The vect is passed by constant reference 
+// and cannot be changed by this function. 
+void func_const_reference(const vector<int> &vect) 
+{ 
+    // vect.push_back(30);  // Uncommenting this line would 
+                             // below error 
+    // "prog.cpp: In function 'void func(const std::vector<int>&)': 
+    // prog.cpp:9:18: error: passing 'const std::vector<int>'  
+    // as 'this' argument discards qualifiers [-fpermissive]"    
+    
+    for (int i=0; i<vect.size(); i++) 
+       cout << vect[i] << " "; 
 } 
    
 int main() 
@@ -17,12 +39,22 @@ int main()
     vect.push_back(10); 
     vect.push_back(20); 
    
-    func(vect); 
+    func_by_value(vect); 
    
     // vect remains unchanged after function 
     // call 
     for (int i=0; i<vect.size(); i++) 
-       cout << vect[i] << " "; 
+       cout << vect[i] << " ";  //10 20
+
+    func_reference(vect);
+
+    cout << endl;
+
+    // vect gets changed after function 
+    // call 
+    for (int i=0; i<vect.size(); i++) 
+       cout << vect[i] << " "; //10 20 30
    
+    func_const_reference(vect);
     return 0; 
 } 
