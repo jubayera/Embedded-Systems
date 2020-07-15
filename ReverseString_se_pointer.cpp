2fad8d621263458162f46f0baae0123748a86bdb
jubayera <cajubuvic@gmail.com>
2020-07-14 19:57:35 -0700

    Create ReverseString_se_pointer.cpp

---
+++ ReverseString_se_pointer.cpp
@@ -0,0 +1,32 @@
+//Reverse String.
+/* Write a function that reverses a string. 
+Do not allocate extra space for another array, you must do this by modifying the 
+input array in-place with O(1) extra memory.*/
+
+class Solution {
+	public:
+		void reverseString(vector<char>& A) {
+			/*
+			stack<char> st;
+			for(int i = 0; i < A.size(); i++) {
+				//pushing every char inside stack
+				st.push(A[i]);
+			}
+			
+			//pop every character
+			for(int i = 0; i < A.size(); i++) {
+				A[i] = st.top();
+				st.pop();
+			}
+			*/
+			int s = 0;
+			int e = A.size() - 1;
+			
+			while(s <= e) {
+				swap(A[s], A[e]);
+				s++;
+				e--;
+			}
+			
+		}
+}
